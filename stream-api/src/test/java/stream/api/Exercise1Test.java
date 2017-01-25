90fe718c9815c507944854f86c5201dc0cc13034
Renaud Trezieres <renaud.trezieres@capgemini.com>
2017-01-25 17:30:10 +0100

    ex1 : howOldAreTheCustomers with reference

--- stream-api/src/test/java/stream/api/Exercise1Test.java
+++ stream-api/src/test/java/stream/api/Exercise1Test.java
@@ -19,37 +19,37 @@ import common.test.tool.entity.Customer;
 import common.test.tool.util.AssertUtil;
 
 public class Exercise1Test extends ClassicOnlineStore {
-    
+
     @Easy
     @Test
     public void findRichCustomers() {
         List<Customer> customerList = this.mall.getCustomerList();
-        
+
         /**
          * Create a {@link Stream} from customerList only including customer who has more budget than 10000. Use lambda expression for Predicate
          * and {@link Stream#filter} for filtering.
          */
         Predicate<Customer> richCustomerCondition = customer -> customer.getBudget() > 10000;
         Stream<Customer> richCustomerStream = customerList.stream().filter(richCustomerCondition);
-        
+
         assertTrue("Solution for Predicate should be lambda expression", AssertUtil.isLambda(richCustomerCondition));
         List<Customer> richCustomer = richCustomerStream.collect(Collectors.toList());
         assertThat(richCustomer, hasSize(2));
         assertThat(richCustomer, contains(customerList.get(3), customerList.get(7)));
     }
-    
+
     @Easy
     @Test
     public void howOldAreTheCustomers() {
         List<Customer> customerList = this.mall.getCustomerList();
-        
+
         /**
          * Create a {@link Stream} from customerList with age values. Use method reference(best) or lambda expression(okay) for creating
          * {@link Function} which will convert {@link Customer} to {@link Integer}, and then apply it by using {@link Stream#map}.
          */
-        Function<Customer, Integer> getAgeFunction = customer -> customer.getAge();
+        Function<Customer, Integer> getAgeFunction = Customer::getAge;
         Stream<Integer> ageStream = customerList.stream().map(getAgeFunction);
-        
+
         assertTrue(AssertUtil.isLambda(getAgeFunction));
         List<Integer> richCustomer = ageStream.collect(Collectors.toList());
         assertThat(richCustomer, hasSize(10));
