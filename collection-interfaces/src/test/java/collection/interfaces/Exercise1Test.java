cc3690b0b5ff959ddb027605edd809ee2ae19965
Renaud Trezieres <renaud.trezieres@capgemini.com>
2017-01-25 11:18:06 +0100

    Exercice1Test : iterateByForEach + whoHaveNoEInYourName

--- collection-interfaces/src/test/java/collection/interfaces/Exercise1Test.java
+++ collection-interfaces/src/test/java/collection/interfaces/Exercise1Test.java
@@ -1,10 +1,7 @@
 package collection.interfaces;
 
-import common.test.tool.annotation.Easy;
-import common.test.tool.dataset.ClassicOnlineStore;
-import common.test.tool.entity.Customer;
-
-import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,50 +13,53 @@ import java.util.function.Predicate;
 import java.util.function.UnaryOperator;
 import java.util.stream.Stream;
 
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.*;
+import org.junit.Test;
+
+import common.test.tool.annotation.Easy;
+import common.test.tool.dataset.ClassicOnlineStore;
+import common.test.tool.entity.Customer;
 
 public class Exercise1Test extends ClassicOnlineStore {
 
-    @Easy @Test
+    @Easy
+    @Test
     public void iterateByForEach() {
         Iterable<Customer> customerIterable = this.mall.getCustomerList();
         List<String> nameList = new ArrayList<>();
 
         /**
-         * Create a {@link Consumer} which represents an operation to add customer's name to {@link nameList} list.
-         * Iterate {@link customerIterable} with {@link Iterable#forEach} and use the {@link Consumer}
-         * to finish creating the name list.
+         * Create a {@link Consumer} which represents an operation to add customer's name to {@link nameList} list. Iterate
+         * {@link customerIterable} with {@link Iterable#forEach} and use the {@link Consumer} to finish creating the name list.
          */
-        Consumer<Object> consumer = null;
-        // customerIterable.
+        Consumer<Customer> consumer = customer -> nameList.add(customer.getName());
+        customerIterable.forEach(consumer);
 
         assertThat(nameList.toString(), is("[Joe, Steven, Patrick, Diana, Chris, Kathy, Alice, Andrew, Martin, Amy]"));
     }
 
-    @Easy @Test
+    @Easy
+    @Test
     public void whoHaveNoEInYourName() {
-        Collection<String> nameCollection =
-            new ArrayList<>(Arrays.asList("Joe", "Steven", "Patrick", "Chris"));
+        Collection<String> nameCollection = new ArrayList<>(Arrays.asList("Joe", "Steven", "Patrick", "Chris"));
 
         /**
-         * Create a {@link Predicate} which predicates if the input string contains "e".
-         * Remove elements from {@link nameCollection} which contains "e" using {@link Collection#removeIf}.
+         * Create a {@link Predicate} which predicates if the input string contains "e". Remove elements from {@link nameCollection} which
+         * contains "e" using {@link Collection#removeIf}.
          */
-        Predicate<Object> predicate = null;
-        // nameCollection.
+        Predicate<String> predicate = (x) -> x.contains("e");
+        nameCollection.removeIf(predicate);
 
         assertThat(nameCollection.toString(), is("[Patrick, Chris]"));
     }
 
-    @Easy @Test
+    @Easy
+    @Test
     public void replaceTheElements() {
-        List<String> nameList =
-            new ArrayList<>(Arrays.asList("Joe", "Steven", "Patrick", "Chris"));
+        List<String> nameList = new ArrayList<>(Arrays.asList("Joe", "Steven", "Patrick", "Chris"));
 
         /**
-         * Create a {@link UnaryOperator} which returns given string wrapped with "()".
-         * Replace the elements in {@link nameList} with string wrapped with "()" using {@link List#replaceAll} .
+         * Create a {@link UnaryOperator} which returns given string wrapped with "()". Replace the elements in {@link nameList} with string
+         * wrapped with "()" using {@link List#replaceAll} .
          */
         UnaryOperator<Object> unaryOperator = null;
         // nameList.
@@ -67,10 +67,10 @@ public class Exercise1Test extends ClassicOnlineStore {
         assertThat(nameList.toString(), is("[(Joe), (Steven), (Patrick), (Chris)]"));
     }
 
-    @Easy @Test
+    @Easy
+    @Test
     public void sortByName() {
-        List<String> nameList =
-            new ArrayList<>(Arrays.asList("Joe", "Steven", "Patrick", "Chris"));
+        List<String> nameList = new ArrayList<>(Arrays.asList("Joe", "Steven", "Patrick", "Chris"));
 
         /**
          * Create a {@link Comparator} to sort the name list by their name's length in ascending order.
@@ -81,14 +81,13 @@ public class Exercise1Test extends ClassicOnlineStore {
         assertThat(nameList.toString(), is("[Joe, Chris, Steven, Patrick]"));
     }
 
-    @Easy @Test
+    @Easy
+    @Test
     public void createStream() {
-        Collection<String> nameList =
-            new ArrayList<>(Arrays.asList("Joe", "Steven", "Patrick", "Chris"));
+        Collection<String> nameList = new ArrayList<>(Arrays.asList("Joe", "Steven", "Patrick", "Chris"));
 
         /**
-         * Create a serial {@link Stream} using {@link Collection#stream}
-         * You can learn about {@link Stream} APIs at stream-api module.
+         * Create a serial {@link Stream} using {@link Collection#stream} You can learn about {@link Stream} APIs at stream-api module.
          */
         Stream<Object> nameStream = null;
 
@@ -96,10 +95,10 @@ public class Exercise1Test extends ClassicOnlineStore {
         assertThat(nameStream.isParallel(), is(false));
     }
 
-    @Easy @Test
+    @Easy
+    @Test
     public void createParallelStream() {
-        Collection<String> nameList =
-            new ArrayList<>(Arrays.asList("Joe", "Steven", "Patrick", "Chris"));
+        Collection<String> nameList = new ArrayList<>(Arrays.asList("Joe", "Steven", "Patrick", "Chris"));
 
         /**
          * Create a parallel {@link Stream} using {@link Collection#parallelStream} or {@link Stream#parallel}
